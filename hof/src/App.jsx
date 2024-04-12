71ece4034e5fc0a5bf33a8e40de73946aad37f7c
Harshitchandel20 <harsitchandel100@.gmail.com>
2024-04-12 18:02:14 +0530

    done

---
+++ hof/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HigherOrderComponent from './Components/HigherOrderComponent'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+     <HigherOrderComponent />
+    </div>
+  )
+}
+
+export default App
